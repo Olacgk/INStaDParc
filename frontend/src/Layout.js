488c8678b3ab7342c038a35287474865afc94882
Jeffrey DAVAKAN <jeffrey.davakan@luxdatadigit.lu>
2023-03-02 18:15:30 +0100

    Fix routes redirections bugs'

---
+++ frontend/src/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Sidebar from './components/sidebar/Sidebar'
+import Topbar from './components/topbar/topbar'
+export default function Layout({children}) {
+    return (
+
+        <div className={'dashboard'}>
+            <Topbar />
+            <div className='containerApp'>
+                <Sidebar />
+                <div className='containerApp_right'>
+                    <Outlet/>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Auth/index.js
+++ frontend/src/pages/Auth/index.js
@@ -1,66 +1,62 @@
-import React, { useState, useRef, useContext } from 'react'
+import React, { useState, useRef, useContext, useEffect } from 'react'
 import './styles.scss'
 import logo from '../../assets/logo_instad.png'
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Auth from '../../context/Auth';
 
 
-export default function Authentification() { 
-
-  const {setAuth} = useContext(Auth);
+export default function Authentification() {
+  
+  const { user, setUser } = useContext(Auth);
   const navigate = useNavigate();
-  const userRef = useRef();
-  const [user, setUser] = useState('');
-  const [pwd, setPwd] = useState('');
+  const emailFieldRef = useRef();
+  const passwordFieldRef = useRef();
   // const [success, setSuccess] = useState(false);
+  useEffect(()=>{
+    if(user) navigate('/');
+  },[])
 
   const [errorMessage, setErrorMessage] = useState(undefined);
   const handleSubmit = (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    
+
     fetch("http://localhost:8080/api/auth/login", {
       method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({"email": email, "password": password})
-  }).then((res) => res.json())
-  .then((data) => {
-      if (data.status === 200) {
-        localStorage.setItem('user', data);
-        const accessToken = data?.accessToken;
-        const roles = data?.roles;
-        setAuth({ user, pwd, roles, accessToken });
-        setUser('');
-        setPwd('');
-        // setSuccess(true);
-        navigate('/');
-      } else {
-        setErrorMessage(data);
-        console.log(errorMessage)
-      }
-  })
-  .catch((err) => console.log(err))
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        "email": emailFieldRef?.current?.value,
+        "password": passwordFieldRef?.current?.value
+      })
+    }).then((res) => res.json())
+      .then((data) => {
+        if (!data.error) {
+          localStorage.setItem('user', JSON.stringify(data));
+          setUser(data);
+          navigate('/');
+        } else {
+          setErrorMessage(data?.error);
+          console.log(data?.error)
+        }
+      })
+      .catch((err) => console.log(err))
   }
 
   return (
     <div className="authentification">
       <div className='authentification_rightpart'>
-        <img src={logo} alt='logo Instad'/>
+        <img src={logo} alt='logo Instad' />
       </div>
       <div className='authentification_leftpart_form'>
-          <form onSubmit={handleSubmit}>
-            <h2 className='authentification_title'>AUTHENTIFICATION</h2>
-            <label htmlFor="email">Email</label>
-            <input type="email" placeholder="email" id='email' ref={userRef} autoComplete="off" onChange={(e) => setUser(e.target.value)} value={user} required/>
-            <label htmlFor="password">Mot de passe</label>
-            <input type="password" placeholder="Mot de passe" id='password' onChange={(e) => setPwd(e.target.value)} value={pwd} required/>
-            <div className='authentication_button'>
-              <button type='submit' value={'submit'} >Se Connecter</button>
-            </div>              
-          </form>
+        <form onSubmit={handleSubmit}>
+          <h2 className='authentification_title'>AUTHENTIFICATION</h2>
+          <label htmlFor="email">Email</label>
+          <input type="email" placeholder="email" id='email' ref={emailFieldRef} autoComplete="off" required />
+          <label htmlFor="password">Mot de passe</label>
+          <input type="password" placeholder="Mot de passe" id='password' ref={passwordFieldRef} required />
+          <div className='authentication_button'>
+            <button type='submit' value={'submit'} >Se Connecter</button>
+          </div>
+        </form>
       </div>
     </div>
   )
